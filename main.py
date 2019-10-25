4577ca864c94fc83851587524fea3357fff2c8da
Alexander Chambers <alexander.chambers8427@gmail.com>
2019-10-26 09:43:50 +1030

    tidier

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main(window_size=(1080, 1080)):
     x = qmvis.space.normal(N_x, dxs)
     dt = 0.01
 
-    wf = qmvis.wavefunction.wavepacket((0.5, 0.25), (-0.5, 0), 1.0, x)
+    wf = qmvis.wavefunction.wavepacket((0.5, 0), (-0.5, 0), 1.0, x)
 
     # V = qmvis.potential.double_slit(x)
     V = qmvis.potential.barrier(0.75, 0.1, 200, x)
