77a845e521a9dbd35a69486f22e08b191b4214ae
Alexander Chambers <alexander.chambers8427@gmail.com>
2019-10-26 09:27:15 +1030

    tidier

--- physvis/wavefunction.py
+++ physvis/wavefunction.py
@@ -4,10 +4,11 @@ import numpy as np
 def wavepacket(p_0, x_0, a, x):
     p_0 = np.asanyarray(p_0)
     x_0 = np.asanyarray(x_0)
+
     norm = 1/(a*np.sqrt(np.pi))
     wf = (
         norm
         *np.exp(-0.5*(((x - x_0)/a)**2).sum(axis=-1))
-        *np.exp(1j*(p_0*(x - x_0)).sum(axis=-1))
+        *np.exp(1j*(p_0*x)).sum(axis=-1))
     )
     return wf
