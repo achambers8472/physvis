e61eeb8174850ef8d51779b7875a7ebe5655a5d2
Alexander Chambers <alexander.chambers8427@gmail.com>
2019-10-26 16:19:02 +1030

    Refactor QuantumParticle slightly

--- physvis/quantum_particle.py
+++ physvis/quantum_particle.py
@@ -8,41 +8,54 @@ def normsq(psi):
 
 
 class QuantumParticle:
-    def __init__(self, x, psi, m=1):
-        self.m = m
-        self.x = x
-        self.psi = psi
-        self.V = np.zeros_like(psi)
-        self.dxs = self.x[1, 1] - self.x[0, 0]
+    def __init__(self, x, psi_x, m=1, V=None):
+        if V is None:
+            V = np.zeros_like(psi_x)
 
+        self.x = x
+        self.psi_x = psi_x
+        self.m = m
+        self.V = V
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        self._x = x
+        self._dxs = self.x[1, 1] - self.x[0, 0]
         Ns = self.x.shape[:-1]
-        self.dks = (2*np.pi)/(Ns*self.dxs)
-        self.k = space.normal(Ns, self.dks)
+        self._dks = (2*np.pi)/(Ns*self._dxs)
+        self.k = space.normal(Ns, self._dks)
         k0 = self.k[0, 0]
 
         self._k0_dot_x = (k0*self.x).sum(axis=-1)
-        self._mod_factor = np.exp(-1j*self._k0_dot_x)*self.dxs[0]/(2*np.pi)
-        self._unmod_factor = np.exp(1j*self._k0_dot_x)*(2*np.pi)/self.dxs[0]
+        self._mod_factor = np.exp(-1j*self._k0_dot_x)*self._dxs[0]/(2*np.pi)
+        self._unmod_factor = np.exp(1j*self._k0_dot_x)*(2*np.pi)/self._dxs[0]
         self._k2 = (self.k**2).sum(axis=-1)
 
+    @property
+    def psi_x(self):
+        return self._psi_mod_x*self._unmod_factor
+
+    @psi_x.setter
+    def psi_x(self, psi_x):
+        self._psi_mod_x = self._mod_factor*psi_x
+
     def update(self, dt):
-        psi_x = self.psi
         x_half_up_factor = np.exp((-0.5j*dt)*self.V)
 
-        psi_mod_x = psi_x*self._mod_factor
-        psi_mod_x *= x_half_up_factor
+        self._psi_mod_x *= x_half_up_factor
 
-        psi_mod_k = np.fft.fft2(psi_mod_x)
-        psi_mod_k *= np.exp((-0.5j*dt/self.m)*self._k2)
-        psi_mod_x = np.fft.ifft2(psi_mod_k)
+        self._psi_mod_k = np.fft.fft2(self._psi_mod_x)
+        self._psi_mod_k *= np.exp((-0.5j*dt/self.m)*self._k2)
+        self._psi_mod_x = np.fft.ifft2(self._psi_mod_k)
 
-        psi_mod_x *= x_half_up_factor
-        psi_x = psi_mod_x*self._unmod_factor
-
-        self.psi = psi_x
+        self._psi_mod_x *= x_half_up_factor
 
     def draw(self, canvas):
-        p = normsq(self.psi)*self.dxs.prod()
+        p = normsq(self.psi_x)*self._dxs.prod()
         canvas.draw_map(p/0.001)
         canvas.draw_map(
             self.V/self.V.max(),
