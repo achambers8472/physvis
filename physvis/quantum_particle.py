11c0c18a6a226fc6c4c8ce5c82cb43a1a7cebffb
Alexander Chambers <alexander.chambers8427@gmail.com>
2019-10-26 16:26:20 +1030

    Start adding code for looking at k-space wavefunction

--- physvis/quantum_particle.py
+++ physvis/quantum_particle.py
@@ -25,23 +25,40 @@ class QuantumParticle:
     def x(self, x):
         self._x = x
         self._dxs = self.x[1, 1] - self.x[0, 0]
+        x0 = self._x[0, 0]
+
         Ns = self.x.shape[:-1]
+
         self._dks = (2*np.pi)/(Ns*self._dxs)
-        self.k = space.normal(Ns, self._dks)
-        k0 = self.k[0, 0]
+        self._k = space.normal(Ns, self._dks)
+        k0 = self._k[0, 0]
 
         self._k0_dot_x = (k0*self.x).sum(axis=-1)
-        self._mod_factor = np.exp(-1j*self._k0_dot_x)*self._dxs[0]/(2*np.pi)
-        self._unmod_factor = np.exp(1j*self._k0_dot_x)*(2*np.pi)/self._dxs[0]
-        self._k2 = (self.k**2).sum(axis=-1)
+
+        self._x_mod_factor = np.exp(-1j*self._k0_dot_x)*self._dxs[0]/(2*np.pi)
+        self._x_unmod_factor = np.exp(1j*self._k0_dot_x)*(2*np.pi)/self._dxs[0]
+
+        self._k_unmod_factor = np.exp(-1j*x0*self._k)
+        self._k_mod_factor = np.exp(-1j*x0*self._k)
+
+        self._k2 = (self._k**2).sum(axis=-1)
 
     @property
     def psi_x(self):
-        return self._psi_mod_x*self._unmod_factor
+        return self._psi_mod_x*self._x_unmod_factor
 
     @psi_x.setter
     def psi_x(self, psi_x):
-        self._psi_mod_x = self._mod_factor*psi_x
+        self._psi_mod_x = self._x_mod_factor*psi_x
+
+    @property
+    def psi_k(self):
+        return self._psi_mod_k*self._k_unmod_factor
+
+    @psi_k.setter
+    def psi_k(self, psi_k):
+        raise NotImplementedError()
+        self._psi_mod_k = self._k_mod_factor*psi_k
 
     def update(self, dt):
         x_half_up_factor = np.exp((-0.5j*dt)*self.V)
